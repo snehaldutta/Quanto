16c3f7e803b1ae7bd15d2201909088a4d4ce9729
Snehal Dutta <snehaldutta1230@gmail.com>
2025-06-13 08:50:59 +0200

    All testcases written

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 from langchain_ollama import ChatOllama
+import tools
+import log
--- tests/test_stock_technicals.py
+++ tests/test_stock_technicals.py
@@ -15,6 +15,36 @@ def test_stock_price_technicals(mock_tick, mock_clean_data):
 
     mock_ins = MagicMock()
     mock_ins.quarterly_financials = mock_df
-    mock_tick.return_value = mock_df
+    mock_tick.return_value = mock_ins
+    mock_clean_data.return_value = mock_df
 
-    
+    res = stock_price_technicals("RVNL.NS")
+
+    assert isinstance(res, pd.DataFrame)
+    assert 'Revenue' in res.columns
+    mock_clean_data.assert_called_once_with(mock_df)
+
+
+@patch('tools.stock_technical_data.log')
+@patch('tools.stock_technical_data.yf.Ticker')
+def test_stock_price_technicals_log(mock_tick, mock_log):
+    mock_ins = MagicMock()
+    mock_ins.quarterly_financials = pd.DataFrame()
+    mock_tick.return_value = mock_ins
+
+    res = stock_price_technicals("RVNL.NS")
+    assert res is None
+    mock_log.logger.warning.assert_called_once()
+
+
+@patch('tools.stock_technical_data.log')
+@patch('tools.stock_technical_data.yf.Ticker')
+def test_stock_price_technicals_except(mock_tick, mock_log):
+    mock_ins = MagicMock()
+    type(mock_ins).quarterly_financials = property(lambda _: (_ for _ in ()).throw(Exception("API Error !!")))
+    mock_tick.return_value = mock_ins
+
+    with pytest.raises(Exception, match="API Error"):
+        stock_price_technicals("RVNL.NS")
+
+    mock_log.logger.exception.assert_called_once()
