0f362e6751e82ba5765c30bc414232c0d74ad5df
Snehal Dutta <snehaldutta1230@gmail.com>
2025-06-14 22:20:17 +0200

    Tools updated

--- tools/stock_data_fetcher.py
+++ tools/stock_data_fetcher.py
@@ -1,41 +1,82 @@
 import yfinance as yf
 import log
-from langchain_core.tools import tool
-@tool
-def stock_price_fetcher(symbol: str) -> str:
-    """Fetches the current price, pe ratio, industry and description of the company """
-    tick = yf.Ticker(symbol)
-    price_attrs : list = ['regularMarketPrice', 'currentPrice', 'price']
+from langchain_core.tools import BaseTool
+from pydantic import BaseModel, field_validator
+from typing import Optional, Type
 
-    try:
-        result : dict = {
-            'current_price':None,
-            'pe_ratio': None,
-            'industry': None,
-            'desc': None
-        }
-        for attr in price_attrs:
-            if result['current_price'] is None and attr in tick.info and tick.info[attr] is not None:
-                result['current_price'] = tick.info[attr]
-            
-        current_price = tick.fast_info.get('last_price')
-        if current_price is not None:
-            result['current_price'] = current_price
-        
-        if 'trailingPE' in tick.info and tick.info['trailingPE'] is not None:
-            result['pe_ratio'] = tick.info['trailingPE']
+class stockPriceFetcherInput(BaseModel):
+    symbol : str = None
 
-        if 'industry' in tick.info and tick.info['industry'] is not None:
-            result['industry'] = tick.info['industry']
+class stockPriceFetcherOutput(BaseModel):
+    current_price : float = None
+    pe_ratio: float = None
+    industry: str = "Unknown"
+    desc: Optional[str] = ""
 
-        if 'longBusinessSummary' in tick.info and tick.info['longBusinessSummary'] is not None:
-            result['desc'] = tick.info['longBusinessSummary'] 
-
-        if result['current_price'] is None:
-            log.logger.warning(f'Price not found for {symbol}')
-        
-        return result
+    @field_validator("current_price")
+    @classmethod
+    def check_current_price(cls, value):
+        if value is not None and value < 0:
+            return log.logger.error("Not able to fetch the value !!")
+        return value
     
-    except Exception as e:
-        log.logger.exception(f"Error fetching data for '{symbol}': {e}")
-        raise
+    @field_validator("pe_ratio")
+    @classmethod
+    def check_pe_ratio(cls, value):
+        if value is not None and value < 0:
+            return log.logger.error("Not able to fetch the value !!")
+        return value
+    
+    @field_validator("industry")
+    @classmethod
+    def check_industry(cls, value):
+        if value is "Unknown":
+            return log.logger.error("Not able to fetch the value !!")
+        return value
+    
+
+class stockPriceFetcherTool(BaseTool):
+    name = "stock_price_fetcher"
+    description = "Fetches the current price, pe ratio, industry and description of the company"
+    args_schema : Type[BaseModel] = stockPriceFetcherInput
+    def _run(self,symbol: str) -> str:
+        tick = yf.Ticker(symbol)
+        price_attrs : list = ['regularMarketPrice', 'currentPrice', 'price']
+
+        try:
+            result : dict = {
+                'current_price':None,
+                'pe_ratio': None,
+                'industry': None,
+                'desc': None
+            }
+            for attr in price_attrs:
+                if result['current_price'] is None and attr in tick.info and tick.info[attr] is not None:
+                    result['current_price'] = tick.info[attr]
+                
+            current_price = tick.fast_info.get('last_price')
+            if current_price is not None:
+                result['current_price'] = current_price
+            
+            if 'trailingPE' in tick.info and tick.info['trailingPE'] is not None:
+                result['pe_ratio'] = tick.info['trailingPE']
+
+            if 'industry' in tick.info and tick.info['industry'] is not None:
+                result['industry'] = tick.info['industry']
+
+            if 'longBusinessSummary' in tick.info and tick.info['longBusinessSummary'] is not None:
+                result['desc'] = tick.info['longBusinessSummary'] 
+
+            if result['current_price'] is None:
+                log.logger.warning(f'Price not found for {symbol}')
+            
+            validated_res = stockPriceFetcherOutput(**result)
+            return validated_res.model_dump_json(indent=2)
+        
+        except Exception as e:
+            log.logger.exception(f"Error fetching data for '{symbol}': {e}")
+            raise
+
+
+    def _arun(self, symbol:str):
+        raise NotImplementedError("Not implemented async yet !!")
--- tools/stock_hist_data.py
+++ tools/stock_hist_data.py
@@ -3,20 +3,48 @@ from tools.technicals import calculate_rsi
 import pandas as pd
 import log
 from data_cleaning import clean_data_history
-from langchain_core.tools import tool
+from langchain_core.tools import BaseTool
+from pydantic import BaseModel, field_validator
+from typing import Type, Any, Dict
+class stockHistDataInput(BaseModel):
+    symbol : str
+class stockHistDataOuput(BaseModel):
+    clean_data: pd.DataFrame = None
 
-@tool
-def stock_hist_data(symbol: str, period:str ='6mo')-> pd.DataFrame:
-    """ It fetches the company's historical data for analysis"""
-    tick = yf.Ticker(symbol)
+    @field_validator
+    @classmethod
+    def check_clean_data(cls,value: pd.DataFrame):
+        if value is not None:
+            not_na = value.isna().sum().sum()
+            if not_na != 0:
+                return log.logger.warning("NAN values present !!")
 
-    try:
-        hist_data = tick.history(period=period)
-        hist_data['EMA_10'] = hist_data['Close'].ewm(span=10,adjust=False).mean()
-        hist_data['RSI_14'] = calculate_rsi(hist_data['Close'],14)
-        clean_hist_data = clean_data_history(hist_data)
-        return clean_hist_data
+        return value
+class stockHistDataTool(BaseTool):
+    name = "stock_hist_data"
+    description = "It fetches the company's historical data for analysis"
+    args_schema: Type[BaseModel] = stockHistDataInput
+    def _run(self, symbol: str, period:str ='6mo')-> Dict[str, Any]:
+
+        tick = yf.Ticker(symbol)
+
+        try:
+            hist_data = tick.history(period=period)
+            hist_data['EMA_10'] = hist_data['Close'].ewm(span=10,adjust=False).mean()
+            hist_data['RSI_14'] = calculate_rsi(hist_data['Close'],14)
+            clean_hist_data = clean_data_history(hist_data)
+
+            validated_data = stockHistDataOuput(clean_data=clean_hist_data)
+            val_data = validated_data.clean_data
+            inp_data = val_data.tail(20).reset_index()
+            return {
+                "markdown_data": inp_data.to_markdown(index=False),
+                'json_data': inp_data.to_json(orient='records')
+            }
+
+        except Exception as e:
+            log.logger.exception(f"Error in fetching the historical data '{symbol}'as {e}")
+            raise
     
-    except Exception as e:
-        log.logger.exception(f"Error in fetching the historical data '{symbol}'as {e}")
-        raise
+    def _arun(self, symbol:str):
+        raise NotImplementedError("Async is not implemented !!")
